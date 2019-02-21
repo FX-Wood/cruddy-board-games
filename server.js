0bb1cdd6495cd75ca96f6bec7258f38f19a799ca
FX-Wood <fxwood2@gmail.com>
2019-02-20 18:09:32 -0800

    commit to move home

--- server.js
+++ server.js
@@ -9,10 +9,30 @@ app.use(express.urlencoded({extended: false}));
 app.use(methodOverride("_method"));
 
 app.get('/', function(req, res) {
-    res.send('this is the root route');
+    res.render('index');
+
 })
 
+app.get('/games', function(req, res) {
+    // find data within data object
+    // send response
+    // res.render
+    
+    
+    db.game.findAll()
+    // try and get all records
+    .then(function(games) {
+        res.render("games/index", {games})
+    })
+})
 
+app.get("/games/:id", function(req, res) {
+    console.log('here')
+    db.game.findById(parseInt(req.params.id))
+    .then(function(game) {
+        res.render('games/show', {game})
+    })
+})
 
 
 app.listen(3000, function() {
