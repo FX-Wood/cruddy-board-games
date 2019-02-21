b540e41e4f792e85049162f22fda5099a69e3411
FX-Wood <fxwood2@gmail.com>
2019-02-21 00:17:19 -0800

    final deliverable

--- server.js
+++ server.js
@@ -11,31 +11,85 @@ app.use(methodOverride("_method"));
 app.get('/', function(req, res) {
     res.render('index');
 
-})
+});
+
+// INDEX
 
 app.get('/games', function(req, res) {
-    // find data within data object
-    // send response
-    // res.render
-    
-    
     db.game.findAll()
-    // try and get all records
     .then(function(games) {
         res.render("games/index", {games})
     })
-})
+});
 
-app.get("/games/:id", function(req, res) {
-    console.log('here')
+// NEW
+
+app.get("/games/new", function(req, res) {
+    res.render("games/create")
+});
+
+// CREATE
+
+app.post("/games", function(req, res) {
+    db.game.create({
+        name: req.body.name,
+        description: req.body.description,
+        players: req.body.players
+    }).then(done => {
+        res.redirect('/games')
+    })
+});
+
+// SHOW
+
+app.get("/games/:id([0-9]+)", function(req, res) {
     db.game.findById(parseInt(req.params.id))
     .then(function(game) {
         res.render('games/show', {game})
     })
-})
+});
 
+// EDIT
+
+app.get("/games/:id([0-9]+)/edit", function(req, res) {
+    console.log('/games/id/edit', req.originalUrl)
+    db.game.findById(parseInt(req.params.id))
+    .then(function(game) {
+        res.render('games/edit', {game})
+    })
+});
+
+// UPDATE
+app.put("/games/:id", function(req, res) {
+    console.log('PUT /games/:id', req.originalUrl);
+    db.game.findOne({
+        where: {id: req.params.id}
+    })
+    .then(game => {
+        console.log(req.body.name, req.body.description, req.body.players)
+        game.update({
+            name: req.body.name,
+            description: req.body.description,
+            players: req.body.players,
+        });
+    })
+    .then(game => {
+        res.redirect("/games/" + req.params.id)
+    })
+});
+
+app.delete('/games/:id', function(req, res) {
+    console.log('DELETE /games/:id')
+    db.game.destroy({
+        where:{
+            id: req.params.id
+        }
+    }).then(
+        res.redirect('/games')
+    )
+});
 
 app.listen(3000, function() {
     console.log('cruddy board games');
     console.log('listening on port 3000');
-})
+});
